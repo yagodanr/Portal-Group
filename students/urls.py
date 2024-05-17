ed505782b303f22aa13e43f56ee03c035882635b
Laros123 <you@example.com>
2024-05-17 15:51:02 +0300

    Add views

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-#from .views import ...
+from .views import UserDetailView, ProjectDetailView, test
 
 urlpatterns = [
+    path('portfolio/<int:pk>', UserDetailView.as_view(), name='portfolio'),
+    path('project/<int:pk>', ProjectDetailView.as_view(), name='project'),
+    path('test', test, name='test'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import DetailView
+from .models import Project
 
 # Create your views here.
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'students/portfolio.html'
+    context_object_name = 'user'
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'students/project.html'
+    context_object_name = 'project'
+
+def test(request):
+    a = Project.objects.get(id=1)
+    for author in a.authors.all():
+        print(author.projects.all())
