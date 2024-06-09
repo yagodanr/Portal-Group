ee019b640163c22483733469a6a50db725151327
yagodanr <yagodanr@gmail.com>
2024-06-09 22:10:38 +0300

    Added freaking filtering with pading, but not sorting

--- gradesheet/urls.py
+++ gradesheet/urls.py
@@ -5,5 +5,5 @@ from .views import Gradesheet, GradesheetSubject
 
 urlpatterns = [
     path('', Gradesheet.as_view(), name="gradesheet"),
-    path('<int:pk>/', GradesheetSubject.as_view(), name="gradesheet_subjec"),
+    path('<int:pk>/', GradesheetSubject.as_view(), name="gradesheet-subject"),
 ] 
--- portal_group/settings.py
+++ portal_group/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'auth_sys',
+    "django_filters",
     "forum",
     "gradesheet",
     'students',
