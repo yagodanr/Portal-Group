3ae5a8bc61a482b1d3133c17e7f59e10125777d6
yagodanr <yagodanr@gmail.com>
2024-06-13 22:31:58 +0300

    Added List and Detail views for Poll

--- poll/urls.py
+++ poll/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from .views import PollListView, PollDetailView
 
 
 urlpatterns = [
+    path("", PollListView.as_view(), name="list-poll"),
+    path("<int:pk>/", PollDetailView.as_view(), name="detail-poll")
 ] 
--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,29 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+
+from .models import Poll, Option
+
 
 # Create your views here.
+class PollListView(ListView):
+    model = Poll
+    context_object_name = 'polls'
+
+class PollDetailView(DetailView):
+    model = Poll
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        
+        total_voted = 0
+        for o in context["poll"].options.all():
+            total_voted += o.voted.all().count()
+
+        percentages = list()
+        for o in context["poll"].options.all():
+            percentages.append(int(o.voted.all().count()* 100 / total_voted)) 
+
+        context["options"] = zip(context["poll"].options.all(), percentages)
+        
+        return context
