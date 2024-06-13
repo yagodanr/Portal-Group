10b9b7f78950b5eb52d2eace25be02a0de2760b5
yagodanr <yagodanr@gmail.com>
2024-06-13 23:59:28 +0300

    Now you can vote in poll

--- poll/views.py
+++ poll/views.py
@@ -1,5 +1,5 @@
 from typing import Any
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Poll, Option
@@ -13,7 +13,7 @@ class PollListView(ListView):
 class PollDetailView(DetailView):
     model = Poll
     
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:    
         context = super().get_context_data(**kwargs)
         
         total_voted = 0
@@ -27,3 +27,18 @@ class PollDetailView(DetailView):
         context["options"] = zip(context["poll"].options.all(), percentages)
         
         return context
+
+    def post(self, request, *args, **kwargs):
+        opt = Option.objects.get(id=request.POST["option"])
+        user = request.user
+        
+        poll = opt.poll
+        for o in poll.options.all():
+            if user in o.voted.all():
+                o.voted.remove(user)
+                break
+        
+        opt.voted.add(request.user)
+        opt.save()
+        
+        return self.get(request)
