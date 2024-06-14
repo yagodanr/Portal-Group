10e1fc05c291a03abd9c40854d035dc8871af7ce
Laros123 <you@example.com>
2024-06-14 22:52:19 +0300

    Add BranchListView and BranchDetailView

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Branch
 
 # Create your views here.
+
+class BranchListView(ListView):
+    model = Branch
+    template_name = 'forum/branch-list.html'
+    context_object_name = 'branches'
+
+class BranchDetailView(DetailView):
+    model = Branch
+    template_name = 'forum/branch-detail.html'
+    context_object_name = 'branch'
