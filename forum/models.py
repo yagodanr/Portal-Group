11ebf08e1222b36b73f6511a10fc52ead4d47ff2
Laros123 <you@example.com>
2024-06-14 20:53:21 +0300

    Add to post foreign key branch

--- forum/models.py
+++ forum/models.py
@@ -9,6 +9,7 @@ class Branch(models.Model):
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='post')
+    branch = models.ForeignKey(Branch, on_delete=models.DO_NOTHING, related_name='post')
     name = models.CharField(max_length=255)
     text = models.TextField()
     media = models.FileField(upload_to='post-media/', blank=True, null=True)
