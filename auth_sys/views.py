7033cc2026776f97fe26a682fd13a453e58c6e81
yagodanr <yagodanr@gmail.com>
2024-06-14 22:49:50 +0300

    Added LogoutView and login to the navbar/header

--- auth_sys/views.py
+++ auth_sys/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView, DetailView, ListView
 
 from .forms import MyUserCreationForm
@@ -13,11 +13,16 @@ class CustomLoginView(LoginView):
     template_name = 'auth_sys/login.html'
     redirect_authenticated_user = True
     
+class CustomLogoutView(LogoutView):
+    ...
+    
+    
 class CustomRegistrationView(CreateView):
     model = MyUser
     form_class = MyUserCreationForm
     template_name="auth_sys/register.html"
     success_url = reverse_lazy('login-form')
+    
     
 
 class UserListView(ListView):
