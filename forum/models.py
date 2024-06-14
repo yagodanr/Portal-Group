63844831246acf6cade3ff001982452f97645547
Laros123 <you@example.com>
2024-06-14 19:13:48 +0300

    New models branch and post

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Branch(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='branch')
+    name = models.CharField(max_length=255)
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name='post')
+    name = models.CharField(max_length=255)
+    text = models.TextField()
+    media = models.FileField(upload_to='post-media/', blank=True, null=True)
