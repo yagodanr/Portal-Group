136a2b38440b7cc552c4dceafe82dae56cf8f06c
Laros123 <you@example.com>
2024-06-21 22:54:07 +0300

    Created CreateView

--- forum/urls.py
+++ forum/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import BranchListView, BranchDetailView
+from .views import BranchListView, BranchDetailView, BranchCreateView, PostCreateView
 
 
 urlpatterns = [
     path('branches/', BranchListView.as_view(), name='branch-list'),
+    path('branch-create/', BranchCreateView.as_view(), name='branch-create'),
     path('branches/<int:pk>', BranchDetailView.as_view(), name='branch-detail'),
+    path('post-create/<int:pk>', PostCreateView.as_view(), name='post-create'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
-from .models import Branch
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .models import Branch, Post
 
 # Create your views here.
 
@@ -13,3 +13,26 @@ class BranchDetailView(DetailView):
     model = Branch
     template_name = 'forum/branch-detail.html'
     context_object_name = 'branch'
+
+class BranchCreateView(CreateView):
+    model = Branch
+    fields = ("name", )
+    template_name = 'forum/branch-create.html'
+
+    def post(self, request, *args, **kwargs):
+        branch = Branch(name=request.POST.get("name"), creator=request.user)
+        branch.save()
+                
+        return redirect("branch-list")
+    
+class PostCreateView(CreateView):
+    model = Post
+    fields = ("name", "text")
+    template_name = 'forum/post-create.html'
+
+    def post(self, request, *args, **kwargs):
+        _branch = get_object_or_404(Branch, pk=self.kwargs.get("pk"))
+        _post = Post(name=request.POST.get("name"), text=request.POST.get("text"), author=request.user, branch=_branch)
+        _post.save()
+                
+        return redirect("branch-detail", self.kwargs.get("pk"))
